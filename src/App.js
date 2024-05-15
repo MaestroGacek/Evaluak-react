4789517c954038234cb935af5fa70890849d09d5
MaestroGacek <kohout.jan@hotmail.cz>
2024-05-15 09:16:04 +0200

    Test

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-import { useEffect, useState } from 'react';
-import axios from 'axios';
-import Paper from '@mui/material/Paper'
-import { Button, Grid, Typography } from '@mui/material';
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Paper from "@mui/material/Paper";
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
 
+const darkTheme = createTheme({
+  palette: {
+      mode: 'dark',
+  },
+});
 
 
 function App() {
@@ -11,6 +19,8 @@ function App() {
   useEffect(() => { axios.get("https://api.sampleapis.com/cartoons/cartoons2D").then(odpoved => { nastavFilm(odpoved.data) }) }, [])
   
   return (
+    <ThemeProvider theme={darkTheme}>
+    <CssBaseline />
 
     <>
     <Typography variant='h2'>Filmy</Typography><br></br>
@@ -27,19 +37,21 @@ function App() {
           <Typography variant='caption'>
               <img src={film.image} style={{ width: "250px",  }}></img><br></br>
               {film.title}<br></br>
-              {film.creator}
-             
+              {film.creator}<br></br>
+              {film.year}
+         
               </Typography>
             </Paper>
           </Grid>
+   
 
           //        <a href={"https://cs.wikipedia.org/w/index.php?search="+props.title+"&title=Speci%C3%A1ln%C3%AD%3AHled%C3%A1n%C3%AD&ns0=1&ns100=1&ns102=1"}
-
-        ))
+))
 
         } </Grid>
-        
+            
     </>
+    </ThemeProvider> 
   );
 
 }
